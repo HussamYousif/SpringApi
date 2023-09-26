66926e629e4ea5b9637b2f60014d03f2a837c9f1
hussam samir yousif <hussam_samir@hotmail.com>
2023-09-26 08:53:58 -0700

    Adds testdata, this will be the response from the repository

---
+++ src/main/java/com/example/springapi/Repositories/TestData.java
@@ -0,0 +1,29 @@
+package com.example.springapi.Repositories;
+
+import com.example.springapi.Models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestData {
+
+    public static List<Product> GetProducts() {
+        var products = new ArrayList<Product>();
+
+        // This could be generated.
+        products.add(new Product("001", "Jordan toothbrush", 26));
+        products.add(new Product("002", "Norwegian potatoes", 5));
+        products.add(new Product("003", "Scented soap", 10));
+        products.add(new Product("004", "Organic shampoo", 15));
+        products.add(new Product("005", "Cotton T-shirt", 20));
+        products.add(new Product("006", "Premium coffee", 35));
+        products.add(new Product("007", "High-end laptop", 1000));
+        products.add(new Product("008", "Comfortable chair", 75));
+        products.add(new Product("009", "Bestseller book", 12));
+        products.add(new Product("010", "Warm socks", 8));
+        products.add(new Product("011", "Cool Axe", 8));
+        products.add(new Product("012", "Spanish Tomatoes",  200));
+
+        return products;
+    }
+}
