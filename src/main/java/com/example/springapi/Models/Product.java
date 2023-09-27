e18ef0f1eaf4c9979d33845e216f9b7801b94a4c
hussam samir yousif <hussam_samir@hotmail.com>
2023-09-27 06:49:30 -0700

    Product model and product repository are added.

--- src/main/java/com/example/springapi/Models/Product.java
+++ src/main/java/com/example/springapi/Models/Product.java
@@ -1,4 +1,23 @@
 package com.example.springapi.Models;
 
-public record Product(String productId, String productName, int price,  String discountId) {
+import java.util.Objects;
+
+public record Product(String productId, String productName, int price, String discountId) {
+
+    public boolean hasDiscount() {
+        return discountId != null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId, product.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
 }
--- src/main/java/com/example/springapi/Repositories/ProductRepository.java
+++ src/main/java/com/example/springapi/Repositories/ProductRepository.java
@@ -1,11 +1,24 @@
 package com.example.springapi.Repositories;
 
+import com.example.springapi.Interfaces.IProductRepository;
 import com.example.springapi.Models.Product;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProductRepository {
-    public List<Product> GetProducts() {
-        return TestData.GetProducts();
+public class ProductRepository implements IProductRepository {
+    public List<Product> GetProducts(List<String> productIds) throws HttpClientErrorException.NotFound {
+        List<Product> results = new ArrayList<>();
+
+        for (String id : productIds) {
+            var current = ProductTestData.GetProducts().stream().filter(p -> p.productId() == id).toList();
+            if (current.isEmpty()) {
+                throw new RuntimeException("ProductId not found: " + productIds + "!");
+            }
+
+            results.add(current.get(0));
+        }
+        return results;
     }
 }
